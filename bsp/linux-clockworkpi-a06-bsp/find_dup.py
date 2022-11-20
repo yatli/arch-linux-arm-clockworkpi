014d8870378e61a566d1a7c86211978d84613ec0
Yatao Li <yatli@microsoft.com>
2022-11-20 19:51:05 +0800

    wip

--- bsp/linux-clockworkpi-a06-bsp/find_dup.py
+++ bsp/linux-clockworkpi-a06-bsp/find_dup.py
@@ -5,7 +5,21 @@ with open('config', 'r') as txt:
         if not line: 
             break
         line = line.strip()
-        conf = line.split('=')[0]
-        if conf in entries and entries[conf] != line:
-            print(f'{entries[conf]}, {line}')
-        entries[conf] = line
+        if not line:
+            print()
+        elif line[0] == '#':
+            if line.endswith(' is not set'):
+                conf = line[2:-11]
+                if conf in entries:
+                    print(f'# [DUP] {conf} is not set')
+                    continue
+                entries[conf] = line
+            print(line)
+        else:
+            conf = line.split('=')[0]
+            if conf in entries:# and entries[conf] != line:
+                # print(f'{entries[conf]}, {line}')
+                print(f'# [DUP] {line}')
+                continue
+            entries[conf] = line
+            print(line)
