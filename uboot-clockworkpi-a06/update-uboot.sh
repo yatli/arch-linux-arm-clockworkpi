f52769f8cb683dabef83a3d7342ad91154a3ab14
Yatao Li <yatli@microsoft.com>
2022-12-06 23:17:21 +0800

    cleanup u-boot; add ocp8178 driver but don't enable that yet

--- uboot-clockworkpi-a06/update-uboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-makepkg -e -f
-sudo pacman -U uboot-clockworkpi-a06-2022.04-3-aarch64.pkg.tar.xz
-sudo dd if=/boot/u-boot.itb of=/dev/mmcblk1 seek=16384 conv=notrunc,fsync 
-sudo dd if=/boot/idbloader.img of=/dev/mmcblk1 seek=64 conv=notrunc,fsync 
-sudo reboot
