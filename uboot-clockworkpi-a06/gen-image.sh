6d84e50068aba148754b781a3c9b32e472eebbc4
Yatao Li <yatli@microsoft.com>
2023-04-04 06:37:29 +0800

    uboot: clean up

--- uboot-clockworkpi-a06/gen-image.sh
+++ uboot-clockworkpi-a06/gen-image.sh
@@ -6,7 +6,7 @@ set -e
 #make clockworkpi-a06-rk3399_defconfig -j6
 #make
 
-cd src/rkbin
+cd src/rkbin-master
 tools/mkimage -n rk3399 -T rksd -d bin/rk33/rk3399_ddr_800MHz_v1.27.bin idbloader.img
 cat bin/rk33/rk3399_miniloader_v1.26.bin >> idbloader.img
 tools/loaderimage --pack --uboot ../u-boot-2022.04/u-boot-dtb.bin uboot.img 0x00200000
