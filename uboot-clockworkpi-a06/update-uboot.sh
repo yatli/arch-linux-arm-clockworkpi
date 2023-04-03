e5e3d4eb8cf0f18b5861f6e35bc837b55a623f62
Yatao Li <yatli@microsoft.com>
2023-04-04 06:39:18 +0800

    uboot: update example install script

--- uboot-clockworkpi-a06/update-uboot.sh
+++ uboot-clockworkpi-a06/update-uboot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+# example script of how to install this package on DevTerm
 set -e
 
-makepkg -e -f
+# scp yatao-rtx:~/home_devterm/arch-linux-arm-clockworkpi-a06/uboot-clockworkpi-a06/uboot-clockworkpi-a06-2022.04-3-aarch64.pkg.tar.xz ./
 sudo pacman -U uboot-clockworkpi-a06-2022.04-3-aarch64.pkg.tar.xz
-sudo dd if=/boot/u-boot.itb of=/dev/mmcblk1 seek=16384 conv=notrunc,fsync 
-sudo dd if=/boot/idbloader.img of=/dev/mmcblk1 seek=64 conv=notrunc,fsync 
+sudo dd if=/boot/idbloader.img of=/dev/mmcblk2 seek=64 conv=notrunc,fsync
+sudo dd if=/boot/uboot.img of=/dev/mmcblk2 seek=16384 conv=notrunc,fsync
+sudo dd if=/boot/trust.img of=/dev/mmcblk2 seek=24576 conv=notrunc,fsync
 sudo reboot
